d720a494a4550a5d937fcdca573a5b0f8fdd6262
Kirti1991 <itsmekeerti.singh@gmail.com>
2021-04-19 11:58:05 +0530

    Material Module creation

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,14 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
+
 import { FormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
-import {MatCardModule} from '@angular/material/card';
-import {MatTooltipModule} from '@angular/material/tooltip';
+import { MaterialModule } from '../shared/materialmodule/materialmodule.module';
+
 
 
 
@@ -21,13 +17,7 @@ import {MatTooltipModule} from '@angular/material/tooltip';
     CommonModule,
     FormsModule,
     DashboardRoutingModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatFormFieldModule,
-    MatIconModule,
-    MatCardModule,
-    MatTooltipModule
+    MaterialModule
     
   ]
 })
